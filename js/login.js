383a0eff70145eeadbafeecc60977c2e08ea683b
luu1504 <hotsuper75@gmail.com>
2026-03-31 18:44:20 +0700

    project

--- js/login.js
+++ js/login.js
@@ -1,53 +1,58 @@
-const usersData = JSON.parse(localStorage.getItem("users")) || [
-   {
-      "id": 1,
-      "fullName": "Admin Chính",
-      "email": "LQTuan@rikkei.edu.vn",
-      "password": "Admin123456",
-      "role": "admin",
-      "createdAt": "2026-03-03T12:26:21.617Z",
-      "isActive": true
-   },
-   {
-      "id": 2,
-      "fullName": "Nguyễn Văn A",
-      "email": "nguyenvana@example.com",
-      "password": "Matkhau123",
-      "role": "user",
-      "createdAt": "2026-03-01T12:26:21.617Z",
-      "isActive": true
-   },
-   {
-      "id": 3,
-      "fullName": "Trần Thị B",
-      "email": "tranthib@example.com",
-      "password": "12345678",
-      "role": "user",
-      "createdAt": "2026-03-03T12:26:21.617Z",
-      "isActive": false
+let usersData = JSON.parse(localStorage.getItem("users")) || [];
+if (usersData.users) {
+   usersData = usersData.users;
+}
+
+if (usersData.length === 0) {
+   usersData = [
+      {
+         "id": 1,
+         "fullName": "Admin Chính",
+         "email": "LQTuan@rikkei.edu.vn",
+         "password": "Admin123456",
+         "role": "admin",
+         "isActive": true
+      },
+      {
+         "id": 2,
+         "fullName": "Nguyễn Văn A",
+         "email": "nguyenvana@example.com",
+         "password": "Matkhau123",
+         "role": "user",
+         "createdAt": "2026-03-01T12:26:21.617Z",
+         "isActive": true
+      },
+      {
+         "id": 3,
+         "fullName": "Trần Thị B",
+         "email": "tranthib@example.com",
+         "password": "12345678",
+         "role": "user",
+         "createdAt": "2026-03-03T12:26:21.617Z",
+         "isActive": false
+      }
+   ];
+}
+
+
+window.onload = () => {
+   const savedEmail = localStorage.getItem("rememberedEmail");
+   const savedPassword = localStorage.getItem("rememberedPassword");
+
+   if (savedEmail && savedPassword) {
+      emailInput.value = savedEmail;
+      passwordInput.value = savedPassword;
+      rememberMe.checked = true;
    }
-];
-let loginForm = document.getElementById("form")
+};
 
-let emailInput = document.getElementById("email");
-let passwordInput = document.getElementById("password");
+function showToast(title, message, type = "success") {
+   let icon = type === "success"
+      ? '<i class="fa-solid fa-circle-check toast-icon"></i>'
+      : '<i class="fa-solid fa-circle-xmark toast-icon"></i>';
 
-let errorEmail = document.getElementById("errorEmail");
-let errorPassword = document.getElementById("errorPassword");
-
-loginForm.addEventListener("submit", (e) => {
-   e.preventDefault();
-
-   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-   const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
-
-   function showToast(title, message, type = "success") {
-      let icon = type === "success"
-         ? '<i class="fa-solid fa-circle-check toast-icon"></i>'
-         : '<i class="fa-solid fa-circle-xmark toast-icon"></i>';
-
-      Toastify({
-         text: `
+   Toastify({
+      text: `
             <div style="display: flex; align-items: center; gap: 12px;">
                 ${icon}
                 <div class="toast-content">
@@ -56,50 +61,82 @@ loginForm.addEventListener("submit", (e) => {
                 </div>
             </div>
         `,
-         duration: 3000,
-         escapeMarkup: false,
-         gravity: "top",
-         position: "right",
-         close: true,
-         className: type === "success" ? "toast-success" : "toast-error",
-         stopOnFocus: true,
-      }).showToast();
-   }
+      duration: 3000,
+      escapeMarkup: false,
+      gravity: "top",
+      position: "right",
+      close: true,
+      className: type === "success" ? "toast-success" : "toast-error",
+      stopOnFocus: true,
+   }).showToast();
+}
 
-   let newEmail = emailInput.value.trim();
-   let newEmailExits = usersData.some(newEmail.toLowerCase() !== data.users.toLowerCase());
-   if (newEmailExits) {
-      errorEmail.innerHTML = "Email không đúng";
+let loginForm = document.getElementById("form");
+let emailInput = document.getElementById("email");
+let passwordInput = document.getElementById("password");
+
+let checkBox = document.getElementById("rememberMe");
+
+let errorEmail = document.getElementById("errorEmail");
+let errorPassword = document.getElementById("errorPassword");
+
+
+loginForm.addEventListener("submit", (e) => {
+   e.preventDefault();
+
+   let authenticEmail = emailInput.value.trim();
+   let authenticPassword = passwordInput.value.trim();
+
+
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+   let findUser = usersData.find(s => s.email === authenticEmail);
+   if (!findUser) {
+      errorEmail.textContent = "Không tìm thấy Email"
+      showToast("Thất bại", "Không tìm thấy email này!", "error");
       return;
    }
-   if (newEmail === "") {
-      errorEmail.innerHTML = "Email không được để trống";
+   if (!emailRegex.test(authenticEmail)) {
+      errorEmail.textContent = "Email không đúng định dạng";
+      showToast("Thất bại", "Định dạng Email không hợp lệ!", "error");
       return;
    }
-   if (!specialCharRegex.test(authenticPassword)) {
-      errorEmail.innerHTML - "Email lỗi định dạng";
+   if (authenticEmail === "") {
+      errorEmail.textContent = "Email không được để trống";
+      showToast("Thất bại", "Email không được để trống!", "error");
+      return;
+   }
+   if (findUser.password !== authenticPassword) {
+      errorPassword.textContent = "Sai mật khẩu"
+      showToast("Thất bại", "Mật khẩu xác nhận không khớp!", "error");
+      return;
+   }
+   if (authenticPassword === "") {
+      errorPassword.textContent = "Mật khẩu không được để trống";
+      showToast("Thất bại", "Mật khẩu Không được để trống", "error");
       return;
    }
 
-   let newPassword = passwordInput.value;
-   let newPasswordExits = usersData.some(s => s.password !== newPassword);
-   if (newPasswordExits) {
-      errorPassword.innerHTML = "Mật khẩu sai";
-      return
-   }
-   if (!specialCharRegex(newPassword)) {
-      errorPassword = "Mật khẩu phải chứa ít nhất một ký tự đặc biệt";
-      return;
-   }
-   if (newPassword === "") {
-      errorPassword = "Mật khẩu không được để trống";
-      return;
-   }
-   if () {
 
+   if (rememberMe.checked) {
+      localStorage.setItem("rememberedEmail", authenticEmail);
+      localStorage.setItem("rememberedPassword", authenticPassword);
+   } else {
+      localStorage.removeItem("rememberedEmail");
+      localStorage.removeItem("rememberedPassword");
    }
 
 
-})
+   showToast("Thành công", "Đặng nhập thàng công , đang chuyển hướng...");
+   localStorage.setItem("currentUser", JSON.stringify(findUser));
 
+
+   errorEmail.textContent = "";
+   errorPassword.textContent = "";
+
+   setTimeout(() => {
+      window.location.href = "index.html";
+   }, 2000);
+
+});
 
--- js/register.js
+++ js/register.js
@@ -1,32 +1,39 @@
-const usersData = JSON.parse(localStorage.getItem("users")) || [
-   {
-      "id": 1,
-      "fullName": "Admin Chính",
-      "email": "LQTuan@rikkei.edu.vn",
-      "password": "Admin123456",
-      "role": "admin",
-      "createdAt": "2026-03-03T12:26:21.617Z",
-      "isActive": true
-   },
-   {
-      "id": 2,
-      "fullName": "Nguyễn Văn A",
-      "email": "nguyenvana@example.com",
-      "password": "Matkhau123",
-      "role": "user",
-      "createdAt": "2026-03-01T12:26:21.617Z",
-      "isActive": true
-   },
-   {
-      "id": 3,
-      "fullName": "Trần Thị B",
-      "email": "tranthib@example.com",
-      "password": "12345678",
-      "role": "user",
-      "createdAt": "2026-03-03T12:26:21.617Z",
-      "isActive": false
-   }
-];
+let usersData = JSON.parse(localStorage.getItem("users")) || [];
+if (usersData.users) {
+   usersData = usersData.users;
+}
+
+if (usersData.length === 0) {
+   usersData = [
+      {
+         "id": 1,
+         "fullName": "Admin Chính",
+         "email": "LQTuan@rikkei.edu.vn",
+         "password": "Admin123456",
+         "role": "admin",
+         "isActive": true
+      },
+      {
+         "id": 2,
+         "fullName": "Nguyễn Văn A",
+         "email": "nguyenvana@example.com",
+         "password": "Matkhau123",
+         "role": "user",
+         "createdAt": "2026-03-01T12:26:21.617Z",
+         "isActive": true
+      },
+      {
+         "id": 3,
+         "fullName": "Trần Thị B",
+         "email": "tranthib@example.com",
+         "password": "12345678",
+         "role": "user",
+         "createdAt": "2026-03-03T12:26:21.617Z",
+         "isActive": false
+      }
+   ];
+}
+
 
 function showToast(title, message, type = "success") {
    let icon = type === "success"
@@ -58,11 +65,15 @@ let emailInput = document.getElementById("email");
 let passwordInput = document.getElementById("password");
 let checkPasswordInput = document.getElementById("checkPassword");
 
+let checkBox = document.getElementById("agreeCheck");
+let loginForm = document.getElementById("form");
 let errorName = document.getElementById("errorName");
 let errorEmail = document.getElementById("errorEmail");
 let errorPassword = document.getElementById("errorPassword");
 let errorConfirm = document.getElementById("confirmPassword");
-let loginForm = document.getElementById("form");
+
+
+
 
 loginForm.addEventListener("submit", (e) => {
    e.preventDefault();
@@ -72,83 +83,100 @@ loginForm.addEventListener("submit", (e) => {
    let authenticPassword = passwordInput.value.trim();
    let checkAuthenticPassword = checkPasswordInput.value.trim();
 
-   let authenticId = usersData.length > 0 ? usersData[usersData.length - 1].id + 1 : 1;
+   let checkAuthenticName = usersData.some(s => s.fullName.toLowerCase() === authenticName.toLowerCase());
+   if (checkAuthenticName) {
+      errorName.textContent = "Tên bị trùng lặp";
+      showToast("Thất bại", "Tên người dùng bị trùng lặp", "error");
+      return;
+   }
+   if (authenticName === "") {
+      errorName.textContent = "Tên người dùng không được để trống";
+      showToast("Thất bại", "Tên người dùng không được để trống", "error");
+      return;
+   }
 
    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-   const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+   const passwordRegex = /^(?=.*[!@#$%^&*(),.?":{}|<>]).+$/;
 
-   errorName.innerHTML = "";
-   errorEmail.innerHTML = "";
-   errorPassword.innerHTML = "";
-   errorConfirm.innerHTML = "";
-
-   let nameExists = usersData.some(s => s.fullName.toLowerCase() === authenticName.toLowerCase());
-   if (authenticName === "") {
-      errorName.innerHTML = "Tên người dùng không được để trống";
-      showToast("Thất bại", "Tên người dùng không được để trống!", "error");
-      return;
-   }
-   if (nameExists) {
-      errorName.innerHTML = "Tên bị trùng lặp";
-      showToast("Thất bại", "Tên này đã tồn tại trên hệ thống!", "error");
-      return;
-   }
-
-   let emailExists = usersData.some(s => s.email.toLowerCase() === authenticEmail.toLowerCase());
+   let checkAuthenticEmail = usersData.some(s => s.email.toLowerCase() === authenticEmail.toLowerCase());
    if (authenticEmail === "") {
-      errorEmail.innerHTML = "Email không được để trống";
-      showToast("Thất bại", "Email không được để trống!", "error");
+      errorEmail.textContent = "Email không được để trống";
+      showToast("Thất bại", "Email người dùng không được phép để trống", "error");
       return;
    }
    if (!emailRegex.test(authenticEmail)) {
-      errorEmail.innerHTML = "Email không đúng định dạng";
-      showToast("Thất bại", "Định dạng Email không hợp lệ!", "error");
+      errorEmail.textContent = "Email không đúng định dạng";
+      showToast("Thất bại", "Email không đúng định dạng", "error");
       return;
    }
-   if (emailExists) {
-      errorEmail.innerHTML = "Email bị trùng lặp";
-      showToast("Thất bại", "Email này đã được sử dụng!", "error");
+   if (checkAuthenticEmail) {
+      errorEmail.textContent = "Email bị trùng lặp";
+      showToast("Thất bại", "Email người dùng bị trùng lặp", "error");
       return;
    }
 
+   let checkAuthenticPasswordList = usersData.some(s => s.password === authenticPassword);
    if (authenticPassword === "") {
-      errorPassword.innerHTML = "Mật khẩu không được để trống";
-      showToast("Thất bại", "Mật khẩu không được để trống!", "error");
+      errorPassword.textContent = "Mật khẩu không được để trống";
+      showToast("Thất bại", "Mật khẩu không được để trống", "error");
+      return;
+   }
+   if (checkAuthenticPasswordList) {
+      errorPassword.textContent = "Mật khẩu đã có người sử dụng";
+      showToast("Thất bại", "Mật khẩu đã có người sử dụng", "error");
+      return;
+   }
+   if (!passwordRegex.test(authenticPassword)) {
+      errorPassword.textContent = "Mật khẩu phải có ít nhất một ký tự đặt biệt";
+      showToast("Thất bại", "Mật khẩu lỗi định dạng", "error");
       return;
    }
    if (authenticPassword.length < 8) {
-      errorPassword.innerHTML = "Mật khẩu phải có tối thiểu 8 ký tự";
-      showToast("Thất bại", "Mật khẩu quá ngắn (tối thiểu 8 ký tự)!", "error");
-      return;
-   }
-   if (!specialCharRegex.test(authenticPassword)) {
-      errorPassword.innerHTML = "Mật khẩu phải chứa ít nhất một ký tự đặc biệt";
-      showToast("Thất bại", "Thiếu ký tự đặc biệt trong mật khẩu!", "error");
+      errorPassword.textContent = "Mật khẩu không được dưới 8 ký tự";
+      showToast("Thất bại", "Mật khẩu người dùng dưới 8 ký tự", "error");
       return;
    }
 
-   if (checkAuthenticPassword !== authenticPassword) {
-      errorConfirm.innerHTML = "Xác nhận mật khẩu lỗi";
-      showToast("Thất bại", "Mật khẩu xác nhận không khớp!", "error");
+   if (authenticPassword !== checkAuthenticPassword) {
+      errorConfirm.textContent = "Mật khẩu xác nhận không đúng";
+      showToast("Thất bại", "Mật khẩu người dùng xác nhận không đúng", "error");
+      return;
+   }
+
+   let newId = usersData.length > 0 ? usersData[usersData.length - 1].id + 1 : 1;
+
+   let isAgreed = checkBox.checked;
+
+   if (!isAgreed) {
+      showToast("Thất bại", "Bạn phải đồng ý điều khoản và dịch vụ", "error");
       return;
    }
 
    let newUser = {
-      id: authenticId,
+      id: newId,
       fullName: authenticName,
       email: authenticEmail,
       password: authenticPassword,
       role: "user",
       createdAt: new Date().toISOString(),
       isActive: true
-   };
+   }
+
 
    usersData.push(newUser);
-   localStorage.setItem("users", JSON.stringify(usersData));
 
-   showToast("Thành công", "Đăng ký tài khoản Cinema thành công!");
+   localStorage.setItem("users", JSON.stringify(usersData));
+   localStorage.setItem("currentUser", JSON.stringify(newUser));
+
+   showToast("Thành công", "Đăng ký thành công, ... Đang chuyển hướng đến trang đăng nhập!");
+
+   errorName.textContent = "";
+   errorEmail.textContent = "";
+   errorPassword.textContent = "";
+   errorConfirm.textContent = "";
 
    setTimeout(() => {
       window.location.href = "login.html";
-   }, 2000);   
+   }, 2000);
 });
+
